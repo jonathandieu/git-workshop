48e4083fdd3e8c14c612981d3ab4d18eb0da6180
jonathanhuydieu <jonathanhuydieu@gmail.com>
2020-12-09 14:53:14 -0500

    Revisiting this repositiory to brush up on my version control/git skills.

--- git.java
+++ git.java
@@ -1,8 +1,8 @@
-
-
- public static void main(String[] args) 
+public class git
 {
+    public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        System.out.println("This might finally work");
     }
 }
+ 
